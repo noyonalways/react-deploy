4a1bc10ae4b4054ba90bdd5e2dddc2c62d9aa59f
noyonalways <noyonrahman2003@gmail.com>
2024-02-28 00:34:40 +0600

    syntax highlighter installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,23 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import SyntaxHighlighter from "react-syntax-highlighter/dist/cjs/prism";
 import "./App.css";
+import { dracula } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const code = `import React from "react"; 
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx"; 
+import "./index.css";
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+  <App />
+</React.StrictMode>
+`;
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <h1>React App Deploy on GitHub pages</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <SyntaxHighlighter language="jsx" style={dracula}>
+        {code}
+      </SyntaxHighlighter>
     </>
   );
 }
